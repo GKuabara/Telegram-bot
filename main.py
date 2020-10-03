b963b8a7472889aa461cff7ac16657d2935f3766
Gabriel <gabrielalveskuabara@gmail.com>
2020-10-03 16:51:37 -0300

    more stuff

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler
+from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters
 
 import logging
 from env import TOKEN
 
 def start(update, context):
-	send = "No worries people, KuBot is HERE"
-    update.message.reply_text(send)
+	send = "No worries people, KuBot has arrived!"
+	update.message.reply_text(send)
 
+def echo(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def caps(update, context):
+	text_caps = ' '.join(context.args).upper()
+	context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)	
+
+def unknown(update, context):
+	context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, invalid command, @{}?".format(update.effective_user.username))
 
 def main():
 	logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-
+	logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+	
 	updater = Updater(token=TOKEN, use_context=True)	
 	dp = updater.dispatcher
 
 	dp.add_handler(CommandHandler("start", start))
+	dp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+	dp.add_handler(CommandHandler("caps", caps))
+	dp.add_handler(MessageHandler(Filters.command, unknown))
 	#commands:
 
 	updater.start_polling()
